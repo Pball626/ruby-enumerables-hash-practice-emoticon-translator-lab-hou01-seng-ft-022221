653082dd7a0318c1e18345fff02000b7530dfc9d
Jose Mendoza <pball626@gmail.com>
2021-02-12 00:25:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,9 +11,14 @@ end
 new_hash
 end
 
-# def get_japanese_emoticon(path, emoticon)
-#   path = YAML.load_file("lib/emoticons.yml")
-# end
+def get_japanese_emoticon(path, emoticon)
+  # code goes here
+  load_library(path).each do |key, value|
+    if value[:english] == emoticon
+      return value[:japanese]
+    end
+  end
+  return "Sorry, that emoticon was not found"
 
 def get_english_meaning
   # code goes here
